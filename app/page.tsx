0847fad3cea5bb183aa06362e140d4c3a223e4e6
Raphaël Badia <raphael@badia.cc>
2023-07-05 11:40:25 +0200

    show that the logger url is truncated

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,10 @@
 /** Add your relevant code here for the issue to reproduce */
-export default function Home() {
-  return null
+export default async function Home() {
+  await fetch(
+    "https://jsonplaceholder.typicode.com/posts/1/comments?url=too_long&truncated=unfortunately_yes"
+  );
+  await fetch(
+    "https://jsonplaceholder.typicode.com/posts/1/comments?url=too_long&hello=false&truncated=unfortunately_yes"
+  );
+  return null;
 }
