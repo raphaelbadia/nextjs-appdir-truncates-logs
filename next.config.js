330d4e7fc5e3799f3828da38d9a2c888caaa42b4
Raphaël Badia <raphael@badia.cc>
2023-11-06 15:00:12 +0100

    use the example from nextjs 14 docs

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import("next").NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  experimental: {
-    appDir: true,
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
   },
-}
+};
